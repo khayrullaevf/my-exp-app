c372ec0a8619a1150687eb8d99dcf7ac334b11bd
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-06-19 16:02:53 +0300

    first commit

---
+++ src/index.mjs
@@ -0,0 +1,53 @@
+import express from 'express';
+
+
+const app=express();
+
+const PORT=process.env.PORT||3000
+
+
+const mockUsers=[
+    {id:1,name:'Fazliddin'},
+    {id:2,name:'Jack'},
+    {id:3,name:'Tony'},
+]
+
+
+
+app.get('/',(req,res)=>{
+    res.status(201).send(mockUsers)
+})
+
+app.get('/api/users',(req,res)=>{
+    res.send([
+        {id:1,name:'Fazliddin'},
+        {id:2,name:'Jack'},
+        {id:3,name:'Tony'},
+    ])
+})
+
+app.get('/api/users/:id',(req,res)=>{
+ const parsedId=parseInt(req.params.id)
+ console.log(parsedId);
+ if (isNaN(parsedId)) {
+    return res.status(400).send({msg:"Bad request"})
+ }else{
+    const findUser=mockUsers.find((user)=>user.id===parsedId)
+    if (!findUser) return res.status(404).send({msg:'User not found'})
+    return res.send(findUser)
+ }
+})
+
+app.get('/api/products',(req,res)=>{
+    res.send(mockUsers)
+})
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Running on port  ${PORT}`);
+})
+
