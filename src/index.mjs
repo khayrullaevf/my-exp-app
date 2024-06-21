0972de5aa1df335ae4ca7c340c85521a4646ec29
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-06-21 07:47:37 +0300

    PUT and PATCH requests

--- src/index.mjs
+++ src/index.mjs
@@ -2,8 +2,12 @@ import express from 'express';
 
 
 const app=express();
+app.use(express.json())
 
 const PORT=process.env.PORT||3000
+app.listen(PORT,()=>{
+    console.log(`Running on port  ${PORT}`);
+})
 
 
 const mockUsers=[
@@ -13,25 +17,34 @@ const mockUsers=[
 ]
 
 
-
 app.get('/',(req,res)=>{
     res.status(201).send(mockUsers)
 })
 
+
 app.get('/api/users',(req,res)=>{
     console.log(req.query);
     const {query:{filter,value }}=req
-    if (!filter&&!value) return res.send(mockUsers)
+
+
     
     if(filter&&value) return res.send(
-    mockUsers.filter((user)=>user[filter].includes(value))
+     mockUsers.filter((user)=>user[filter].includes(value))
     )
-
-
-
-
+    return res.send(mockUsers)
 
 })
+
+app.post('/api/users',(req,res)=>{
+    console.log(req.body);
+    const {body}=req
+    const newUser={id:mockUsers[mockUsers.length-1].id+1,...body}
+    mockUsers.push(newUser)
+    return res.status(201).send(newUser)
+
+   
+})
+
 
 app.get('/api/users/:id',(req,res)=>{
  const parsedId=parseInt(req.params.id)
@@ -51,10 +64,40 @@ app.get('/api/products',(req,res)=>{
 
 
 
+app.put('/api/users/:id',(req,res)=>{
+    const{
+    body,
+    params:{id}
+    }=req
+    
+  const parsedId=parseInt(id)
+
+  if (isNaN(parsedId)) return res.sendStatus(400)
+
+  const findUserIndex=mockUsers.findIndex((user)=>user.id===parsedId)
+
+  if (findUserIndex===-1) return res.sendStatus(404)
+   mockUsers[findUserIndex]={id:parsedId,...body}
+   return res.sendStatus(200)
+
+  
 
 
 
-app.listen(PORT,()=>{
-    console.log(`Running on port  ${PORT}`);
+
+
+
+
 })
+
+
+
+
+
+
+
+
+//PUT
+//PATCH
+//DELETE 
 
