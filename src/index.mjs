7f1039b8f0b4ba8fc87c10c40cc2edb753defcb5
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-06-21 09:02:03 +0300

    middleware

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,27 @@ import express from 'express';
 const app=express();
 app.use(express.json())
 
+const loggingMiddleware=(req,res,next)=>{
+    console.log(req.method);
+    console.log(req.url);
+    next()
+}
+const resolveIndexByUserId=(req,res,next)=>{
+    const{
+        params:{id}
+        }=req 
+      const parsedId=parseInt(id)
+      if (isNaN(parsedId)) return res.sendStatus(400)
+      const findUserIndex=mockUsers.findIndex((user)=>user.id===parsedId)
+      if (findUserIndex===-1) return res.sendStatus(404)
+      req.findUserIndex=findUserIndex
+      next()
+}
+
+app.use(loggingMiddleware,(req,res,next)=>{
+    console.log('finished logging..');
+    next()
+})
 const PORT=process.env.PORT||3000
 app.listen(PORT,()=>{
     console.log(`Running on port  ${PORT}`);
@@ -23,11 +44,7 @@ app.get('/',(req,res)=>{
 
 
 app.get('/api/users',(req,res)=>{
-    console.log(req.query);
     const {query:{filter,value }}=req
-
-
-    
     if(filter&&value) return res.send(
      mockUsers.filter((user)=>user[filter].includes(value))
     )
@@ -35,27 +52,22 @@ app.get('/api/users',(req,res)=>{
 
 })
 
+
+
 app.post('/api/users',(req,res)=>{
-    console.log(req.body);
     const {body}=req
-    const newUser={id:mockUsers[mockUsers.length-1].id+1,...body}
+    const newUser={id:mockUsers[mockUsers.length-1].id+1||1,...body}
     mockUsers.push(newUser)
     return res.status(201).send(newUser)
-
-   
 })
 
 
-app.get('/api/users/:id',(req,res)=>{
- const parsedId=parseInt(req.params.id)
- console.log(parsedId);
- if (isNaN(parsedId)) {
-    return res.status(400).send({msg:"Bad request"})
- }else{
-    const findUser=mockUsers.find((user)=>user.id===parsedId)
+app.get('/api/users/:id',resolveIndexByUserId,(req,res)=>{
+    const {findUserIndex}=req
+    const findUser=mockUsers[findUserIndex]
     if (!findUser) return res.status(404).send({msg:'User not found'})
     return res.send(findUser)
- }
+ 
 })
 
 app.get('/api/products',(req,res)=>{
@@ -64,35 +76,24 @@ app.get('/api/products',(req,res)=>{
 
 
 
-app.put('/api/users/:id',(req,res)=>{
-    const{
-    body,
-    params:{id}
-    }=req
-    
-  const parsedId=parseInt(id)
-
-  if (isNaN(parsedId)) return res.sendStatus(400)
-
-  const findUserIndex=mockUsers.findIndex((user)=>user.id===parsedId)
-
-  if (findUserIndex===-1) return res.sendStatus(404)
-   mockUsers[findUserIndex]={id:parsedId,...body}
-   return res.sendStatus(200)
-
-  
-
-
-
-
-
-
-
+app.put('/api/users/:id',resolveIndexByUserId,(req,res)=>{
+    const{body, findUserIndex}=req 
+    mockUsers[findUserIndex]={id:mockUsers[findUserIndex].id,...body}
+    return res.sendStatus(200)
 })
 
 
+app.patch('/api/users/:id',resolveIndexByUserId,(req,res)=>{
+const{body, findUserIndex}=req 
+mockUsers[findUserIndex]={...mockUsers[findUserIndex], ...body}
+return res.sendStatus(200)
 
-
+})
+app.delete('/api/users/:id',resolveIndexByUserId,(req,res)=>{
+    const{findUserIndex}=req
+    mockUsers.splice(findUserIndex,1)
+    return res.sendStatus(200)
+})
 
 
 
